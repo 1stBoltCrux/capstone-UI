64b3d7b01873fc55aa7b17f77fdac03b08205aa2
Student <example@example.com>
2018-06-19 14:21:03 -0700

    add react app

---
+++ ozone/src/components/Home.js
@@ -0,0 +1,12 @@
+import  React from 'react';
+import ReactDOM from 'react-dom';
+
+class Home extends React.Component {
+  render () {
+    return (
+      <p>Home is working</p>
+    )
+  }
+}
+
+export default Home;
