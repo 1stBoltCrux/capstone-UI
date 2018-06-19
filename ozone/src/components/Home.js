5150ec101bb1d6704f63703b2e3e308466bccc78
Student <example@example.com>
2018-06-19 15:44:59 -0700

    continue homepage css

--- ozone/src/components/Home.js
+++ ozone/src/components/Home.js
@@ -10,7 +10,16 @@ class Home extends React.Component {
         <div className='nav'>
           <div className='nav-button'><p>My List</p></div>
           <div className='nav-button'>Route List</div>
+        </div>
+        <div className='filters'>
+          <div className='filter-route'>
+            <div className='filter-box' id="filter-title"><p>Filter Routes</p></div>
+          <div className='filter-box'><p>name:</p><input type='text'></input></div>
+          <div className='filter-box'><p>grade:</p><select></select></div>
+          <div className='filter-box'><p>type:</p><select></select></div>
+        </div>
 
+          <div className='filter-my-list'></div>
         </div>
 
     </div>
