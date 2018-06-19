75ab3eb0d30a54512971fd9fd3163baf7519c2b2
Student <example@example.com>
2018-06-19 14:46:08 -0700

    add top logo for home.js

--- ozone/src/components/Home.js
+++ ozone/src/components/Home.js
@@ -1,10 +1,19 @@
 import  React from 'react';
 import ReactDOM from 'react-dom';
+import './home.css';
 
 class Home extends React.Component {
   render () {
     return (
-      <p>Home is working</p>
+      <div className='home-wrapper'>
+
+        <div className='nav'>
+          <div className='nav-button'><p>My List</p></div>
+          <div className='nav-button'>Route List</div>
+
+        </div>
+
+    </div>
     )
   }
 }
