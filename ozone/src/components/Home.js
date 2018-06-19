689579dfcb330f04ca56269b207a72ed235d56fd
Student <example@example.com>
2018-06-19 16:07:59 -0700

    finish draft homepage

--- ozone/src/components/Home.js
+++ ozone/src/components/Home.js
@@ -14,12 +14,21 @@ class Home extends React.Component {
         <div className='filters'>
           <div className='filter-route'>
             <div className='filter-box' id="filter-title"><p>Filter Routes</p></div>
-          <div className='filter-box'><p>name:</p><input type='text'></input></div>
-          <div className='filter-box'><p>grade:</p><select></select></div>
-          <div className='filter-box'><p>type:</p><select></select></div>
-        </div>
+            <div className='filter-box'><p>name:</p><input type='text'></input></div>
+            <div className='filter-box'><p>grade:</p><select></select></div>
+            <div className='filter-box'><p>type:</p><select></select></div>
+          </div>
+          <div className='filter-route-button'><p>Filter Route List</p></div>
 
-          <div className='filter-my-list'></div>
+          <div className='filter-my-list'>
+            <div className='filter-route'>
+              <div className='filter-box' id="filter-title"><p>Filter My List</p></div>
+            <div className='filter-box'><p>name:</p><input type='text'></input></div>
+            <div className='filter-box'><p>grade:</p><select></select></div>
+            <div className='filter-box'><p>type:</p><select></select></div>
+          </div>
+          </div>
+          <div className='filter-route-button'><p>Filter My List</p></div>
         </div>
 
     </div>
