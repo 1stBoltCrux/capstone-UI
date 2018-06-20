11ee77b30e372d7e312be1af5e485429a1c49cd6
Student <example@example.com>
2018-06-20 11:48:35 -0700

    add notes field and ok button

--- ozone/src/components/EditModal.js
+++ ozone/src/components/EditModal.js
@@ -1,14 +1,26 @@
 import  React from 'react';
 import ReactDom from 'react-dom';
 import './editmodal.css'
+import okButton from './../imgs/okbutton.svg';
+
 
 class EditModal extends React.Component {
   render() {
     return (
       <div className='edit-modal-wrapper'>
-        <div className='bottom-logo'>
-          
+        <div className='modal-content'>
+        <div className='top-bar'>
+          <h3>Place Holder</h3>
+          <div className='close-button'></div>
         </div>
+        <div className='content-container'>
+          <div className='notes-container'>
+
+          </div>
+          <img src={okButton}/>
+        </div>
+      </div>
+        <div className='bottom-logo'></div>
 
       </div>
     );
