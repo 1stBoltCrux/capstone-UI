007c82e25ccb356d1a3c35a38e9f95f4aacc24cf
Student <example@example.com>
2018-06-20 11:20:21 -0700

    start on edit modal

--- ozone/src/App.js
+++ ozone/src/App.js
@@ -5,6 +5,7 @@ import  Home from './components/Home.js';
 import List from './components/List.js';
 import  Nav from './components/Nav.js';
 import DetailPage from './components/DetailPage.js';
+import  EditModal from './components/EditModal.js';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
         <Home></Home>
         <List></List>
         <DetailPage></DetailPage>
+        <EditModal></EditModal>
 
       </div>
     );
---
+++ ozone/src/components/EditModal.js
@@ -0,0 +1,18 @@
+import  React from 'react';
+import ReactDom from 'react-dom';
+import './editmodal.css'
+
+class EditModal extends React.Component {
+  render() {
+    return (
+      <div className='edit-modal-wrapper'>
+        <div className='bottom-logo'>
+          
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default EditModal;
