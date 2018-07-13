6b1461ba4bf2fa09a89f426eeeb872293bd8e487
Ted Leary <markeleary@gmail.com>
2018-07-13 09:19:50 -0700

    add css modules syntax to nav, detail page

--- ozone/src/components/DetailPage.js
+++ ozone/src/components/DetailPage.js
@@ -1,36 +1,37 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import './detailpage.css';
+import styles from './detailpage.css';
 import Nav from './Nav.js'
 import star from './../imgs/icon-star.svg'
 import moreInfo from './../imgs/icon-more-info.svg'
 import {Link} from 'react-router-dom';
+
 class DetailPage extends React.Component {
   render() {
     return (
-      <div className='detail-page-wrapper'>
+      <div className={styles.detailPageWrapper}>
         <Nav></Nav>
-        <div className='detail-info-box'>
-            <div className='detail'><h3>The Crumbling</h3></div>
-            <div className='detail'><p>5.12a</p></div>
-            <div className='detail'><p>Mordor Wall</p></div>
-            <div className='detail'><p><span className='bright-green'>Pitches: 1</span></p></div>
-            <div className='detail'><img src={star}/></div>
-            <div className='detail'><img src={moreInfo}/></div>
+        <div className={styles.detailInfoBox}>
+            <div className={styles.detail}><h3>The Crumbling</h3></div>
+            <div className={styles.detail}><p>5.12a</p></div>
+            <div className={styles.detail}><p>Mordor Wall</p></div>
+            <div className={styles.detail}><p><span className={styles.brightGreen}>Pitches: 1</span></p></div>
+            <div className={styles.detail}><img src={star}/></div>
+            <div className={styles.detail}><img src={moreInfo}/></div>
         </div>
-        <div className='route-image'>
+        <div className={styles.routeImage}>
 
 
         </div>
-        <div className='button-backdrop'>
-          <div className='top-buttons'>
-            <Link to="/editmodal"><div className='top-button1'><p><span className='bright-green'>Grade</span></p></div></Link>
-            <Link to="/editmodal"><div className='top-button2'><p><span className='bright-green'>Complete</span></p></div></Link>
-            <Link to="/editmodal"><div className='top-button3'><p><span className='bright-green'>Notes</span></p></div></Link>
+        <div className={styles.buttonBackdrop}>
+          <div className={styles.topButtons}>
+            <Link to="/editmodal"><div className={styles.topButton1}><p><span className={styles.brightGreen}>Grade</span></p></div></Link>
+            <Link to="/editmodal"><div className={styles.topButton2}><p><span className={styles.brightGreen}>Complete</span></p></div></Link>
+            <Link to="/editmodal"><div className={styles.topButton3}><p><span className={styles.brightGreen}>Notes</span></p></div></Link>
           </div>
-          <div className='bottom-buttons'>
-            <Link to="/editmodal"><div className='bottom-button1'><p><span className='bright-green'>Add to List</span></p></div></Link>
-            <Link to="/editmodal"><div className='bottom-button2'><p><span className='bright-green'>Rate</span></p></div></Link>
+          <div className={styles.bottomButtons}>
+            <Link to="/editmodal"><div className={styles.bottomButton1}><p><span className={styles.brightGreen}>Add to List</span></p></div></Link>
+            <Link to="/editmodal"><div className={styles.bottomButton2}><p><span className={styles.brightGreen}>Rate</span></p></div></Link>
           </div>
 
         </div>
--- ozone/src/components/Nav.js
+++ ozone/src/components/Nav.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import './nav.css'
+import styles from './nav.css'
 import {Link} from 'react-router-dom';
 
 class List extends React.Component {
   render() {
     return (
-      <div className='nav-wrapper'>
-        <div className='nav'>
-          <Link to ="/list"><div className='nav-button'>My List</div></Link>
-          <Link to ="/"><div className='nav-button'>Home</div></Link>
-          <Link to ="/list"><div className='nav-button'>Route List</div></Link>
+      <div className={styles.navWrapper}>
+        <div className={styles.nav}>
+          <Link to ="/list"><div className={styles.navButton}>My List</div></Link>
+          <Link to ="/"><div className={styles.navButton}>Home</div></Link>
+          <Link to ="/list"><div className={styles.navButton}>Route List</div></Link>
         </div>
       </div>
     );
