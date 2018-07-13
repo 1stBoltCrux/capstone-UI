248a046b05853a1c8ca48613664327e10d21f37c
Ted Leary <markeleary@gmail.com>
2018-07-13 14:46:13 -0700

    add uuid

--- ozone/src/components/List.js
+++ ozone/src/components/List.js
@@ -3,18 +3,20 @@ import ReactDom from 'react-dom';
 import styles from './list.css';
 import Nav from './Nav.js';
 import ListItem from './ListItem.js';
+import { v4 } from 'uuid';
 
 class List extends React.Component {
   render() {
     console.log(this.props);
     return (
       <div className={styles.listWrapper}>
-        {this.props.routes.map((route) =>
+        {this.props.routes.map((route, key) =>
 
           <ListItem
           name={route.name}
           rating={route.rating}
           location={route.location[route.location.length-1]}
+          key={v4()}
           />
         )}
 
