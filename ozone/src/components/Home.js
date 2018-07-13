e02616bbce41db2a573a6f58806121206cc5f56b
Ted Leary <markeleary@gmail.com>
2018-07-13 09:00:10 -0700

    start refactoring for css modules

--- ozone/src/components/Home.js
+++ ozone/src/components/Home.js
@@ -1,30 +1,30 @@
 import  React from 'react';
 import ReactDOM from 'react-dom';
-import './home.css';
+import styles from './home.css'
 
 
 class Home extends React.Component {
   render () {
     return (
-      <div className='home-wrapper'>
-        <div className='filters'>
-          <div className='filter-route'>
-            <div className='filter-box' id="filter-title"><p>Filter Routes</p></div>
-            <div className='filter-box'><p>name:</p><input type='text'></input></div>
-            <div className='filter-box'><p>grade:</p><select></select></div>
-            <div className='filter-box'><p>type:</p><select></select></div>
+      <div className={styles.homeWrapper}>
+        <div className={styles.filters}>
+          <div className={styles.filterRoute}>
+            <div className={styles.filterBox} id="filter-title"><p>Filter Routes</p></div>
+            <div className={styles.filterBox}><p>name:</p><input type='text'></input></div>
+            <div className={styles.filterBox}><p>grade:</p><select></select></div>
+            <div className={styles.filterBox}><p>type:</p><select></select></div>
           </div>
-          <div className='filter-route-button'><p>Filter Route List</p></div>
+          <div className={styles.filterRouteButton}><p>Filter Route List</p></div>
 
-          <div className='filter-my-list'>
-            <div className='filter-route'>
-              <div className='filter-box' id="filter-title"><p>Filter My List</p></div>
-            <div className='filter-box'><p>name:</p><input type='text'></input></div>
-            <div className='filter-box'><p>grade:</p><select></select></div>
-            <div className='filter-box'><p>type:</p><select></select></div>
+          <div className={styles.filterMyList}>
+            <div className={styles.filterRoute}>
+              <div className={styles.filterBox} id="filter-title"><p>Filter My List</p></div>
+            <div className={styles.filterBox}><p>name:</p><input type='text'></input></div>
+            <div className={styles.filterBox}><p>grade:</p><select></select></div>
+            <div className={styles.filterBox}><p>type:</p><select></select></div>
           </div>
           </div>
-          <div className='filter-route-button'><p>Filter My List</p></div>
+          <div className={styles.filterRouteButton}><p>Filter My List</p></div>
         </div>
 
     </div>
