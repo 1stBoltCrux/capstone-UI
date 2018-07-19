2539f72cf159c007546ac4d6dd00f1e242e52e9c
ted <Guest@Epicodus-10D.local>
2018-07-18 18:49:04 -0700

    change api address to return ozone specific routes

--- ozone/src/actions/index.js
+++ ozone/src/actions/index.js
@@ -4,7 +4,7 @@ const { types } = constants
 export default function makeCall() {
   let data;
   return dispatch => {
-    fetch('https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=40.03&lon=-105.25&maxDistance=10&minDiff=5.6&maxDiff=5.10&key=200285890-fbff6471f00c42d4b58bbfed57cd6a12').then(
+    fetch('https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=45.567&lon=-122.211&maxDistance=5&minDiff=5.6&maxDiff=5.10&key=200285890-fbff6471f00c42d4b58bbfed57cd6a12').then(
       response => response.json()
     ).then(function(json){
       data = json
