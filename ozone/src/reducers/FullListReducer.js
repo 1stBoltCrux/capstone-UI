c2c08e9805b6043adc074aaae28affe3fbbf0599
ted <Guest@Epicodus-10D.local>
2018-07-19 08:54:14 -0700

    add sort function to fullListReducer, sorts by name of route

--- ozone/src/reducers/FullListReducer.js
+++ ozone/src/reducers/FullListReducer.js
@@ -5,6 +5,15 @@ const FullListReducer = (state = {}, action) => {
   switch(action.type) {
     case types.RECEIVED: {
       const fullList = Object.assign({}, action.payload);
+      function compare(a,b){
+        if (a.name < b.name)
+        return -1;
+        if (a.name > b.name)
+        return 1;
+        return 0;
+      }
+      fullList.routes.sort(compare);
+      console.log(fullList);
       return fullList;
     }
   }
