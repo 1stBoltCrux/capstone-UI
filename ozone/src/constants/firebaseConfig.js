78bf494c6b4f0bd98f22968e0a6ddc50a1a93a4a
tedLeary <markeleary@gmail.com>
2018-09-09 20:36:36 -0700

    add in firebaseConfig

---
+++ ozone/src/constants/firebaseConfig.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+   apiKey: "AIzaSyB4dZV8BWfwoyxObrHgxwUUjKhAYJLQxdQ",
+   authDomain: "ozone-4094a.firebaseapp.com",
+   databaseURL: "https://ozone-4094a.firebaseio.com",
+   projectId: "ozone-4094a",
+   storageBucket: "ozone-4094a.appspot.com",
+   messagingSenderId: "1053551033368"
+ };
+export default firebaseConfig
